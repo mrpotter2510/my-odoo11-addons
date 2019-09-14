20671ed19c4f7a2eca33f94e693e023df3c54c86
Phạm Hoàng Giang <phamgiang2510@gmail.com>
2019-09-14 22:44:30 +0700

    first commit

---
+++ ir_sequence_auto_reset/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Ir Sequence Auto Reset",
+
+    'summary': """
+        Ir Sequence Auto Reset
+        """,
+
+    'description': """
+        Auto reset your sequence daily, monthly, yearly or your custom period.
+    """,
+
+    'author': "mrpotter2510",
+    'version': '11.0',
+    'depends': ['base'],
+    'application': False,
+    'installable': True,
+    'data': [
+        'views/ir_sequence_view.xml',
+    ],
+}
